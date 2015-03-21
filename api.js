d5cecafa9fdeb035f2761e2f3dc527aca7963226
yanlong <scicodist@gmail.com>
2015-03-21 21:07:52 +0800

    Add router; add api.

---
+++ api.js
@@ -0,0 +1,12 @@
+Router.route('/api/topics/:id?', {where: 'server'})
+.get(function () {
+    var id = this.params.id
+    var topic = id ? Topics.findOne({_id:id}) : Topics.find().fetch();
+    if (!topic) {
+        throw new Meteor.Error('No such topic, id:'+id)
+    }
+    this.response.end(JSON.stringify(topic));
+})
+.post(function () {
+    this.response.end('post request\n');
+});
