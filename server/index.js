e52cac8553da606aab68689f89641168d87378c9
yanlong <scicodist@gmail.com>
2015-03-24 17:54:11 +0800

    Add more apis

--- server/index.js
+++ server/index.js
@@ -8,17 +8,37 @@ if (Meteor.isServer) {
 
         // Global configuration
         Restivus.configure({
-          useAuth: true
-      });
+            useAuth: true
+        });
 
 
         Restivus.addCollection(Meteor.users);
 
         Restivus.addCollection(Topics, {
             routeOptions: {
-                authRequired: true,
-                roleRequired: 'admin'
+                // authRequired: true,
+                // roleRequired: 'admin'
             },
+            endpoints: {
+                getAll: {
+                    // authRequired: true,
+                    // roleRequired: 'admin'
+
+                },
+                get: {
+                    authRequired: true,
+                    // roleRequired: 'admin'
+                }
+            }
         });
+        Restivus.addRoute('topics/:topicId/bets/:betId?', {}, {
+            get: function() { 
+                var option = {
+                    topic: this.params.topicId,
+                    _id: this.params.betId,
+                }
+                return this.params.betId == null ? Bets.find(option).fetch() : (Bets.findOne(option)||{});
+            }
+        })
     });
 }
