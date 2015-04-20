34f8a747a74d70ac7afe9f8e9f10080a71efc8bf
yanlong <scicodist@gmail.com>
2015-04-20 12:58:22 +0800

    Add validation for insert topic

--- client/tpl/add_topic.js
+++ client/tpl/add_topic.js
@@ -1,11 +1,16 @@
 Template.addTopic.events({
     'submit form': function(e) {
         e.preventDefault();
-        var topic = {
-        };
-        Schemas.topic.forEach(function(v) {
-                topic[v.name] = $(e.target).find('[name=' + v.name + ']').val();
-            })
+        var topic = {};
+        for (var i in Schemas.topic) {
+            var v = Schemas.topic[i];
+            var value = $(e.target).find('[name=' + v.name + ']').val();
+            if (!value && !v.optional) {
+                alert('Invaild value: ' + v.name);
+                return;
+            }
+            topic[v.name] = value;
+        }
         topic.user = Meteor.userId();
         var id = Topics.insert(topic);
         Router.go('topicList');
