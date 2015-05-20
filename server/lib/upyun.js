e339934aa9983d66d0587fef1fa5c4ce68d5867b
yanlong <scicodist@gmail.com>
2015-05-20 17:49:54 +0800

    Add upyun sdk

---
+++ server/lib/upyun.js
@@ -0,0 +1,9 @@
+var UPYUN = Meteor.require('upyun');
+
+var upyun = new UPYUN('caogen-images', 'topspot', 'caogen123', 'ctcc', 'legacy');
+
+upyun.uploadFile('/test' , './upyun.js', 'text/plain', true, function(err, result) {
+    //...
+    console.log(result);
+})
+
