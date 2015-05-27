93d4aabdb7df89081cb04b0842259f7101986464
yanlong <scicodist@gmail.com>
2015-05-27 17:45:44 +0800

    Add close topic for console.

--- client/templates/topics/topic_list_item.js
+++ client/templates/topics/topic_list_item.js
@@ -4,6 +4,12 @@ Template.topicListItem.events({
         // alert('ffff'+this._id);
         if (confirm('确定删除？'))
             Topics.remove(this._id);
+    },
+    'click .topic-close': function(e) {
+        e.preventDefault();
+        // alert('ffff'+this._id);
+        if (confirm('结束话题后会自动为用户结算【点劵】，且不能重新开启话题，确定结束？'))
+            Topics.update(this._id, {$set: {status:'close'}});
     }
 })
 Template.topicListItem.helpers({
