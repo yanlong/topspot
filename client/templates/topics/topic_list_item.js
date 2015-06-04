44f7caf442c3efa61aaaab2687e0d0e1285dc7aa
yanlong <scicodist@gmail.com>
2015-06-04 20:50:43 +0800

    Add close option

--- client/templates/topics/topic_list_item.js
+++ client/templates/topics/topic_list_item.js
@@ -1,17 +1,19 @@
 Template.topicListItem.events({
     'click .remove': function(e) {
         e.preventDefault();
-        // alert('ffff'+this._id);
         if (confirm('确定删除？'))
             Topics.update(this._id, {$set:{status:'deleted'}});
     },
     'click .topic-close': function(e) {
         e.preventDefault();
-        // alert('ffff'+this._id);
-        if (confirm('结束话题后会自动为用户结算【点劵】，且不能重新开启话题，确定结束？'))
-            Topics.update(this._id, {$set: {status:'close'}});
+        if (confirm('结束话题后会自动为用户结算【点劵】，且不能重新开启话题，确定结束？')) {
+            var val = parseInt($(e.target).html());
+            Topics.update(this._id, {$set: {status:'close', price: val}});
+        }
     }
 })
 Template.topicListItem.helpers({
-    // types: Schemas.topic,
+    isOpen: function () {
+        return this.status !== 'close';
+    }
 })
